3c6714bc1ad85a358a2f945a9d17cbf676aa41f7
cryx <cryx@1bf41d8a-299c-db11-a312-0030488a8c24>
2009-02-10 19:39:01 +0000

    The zfs on the backup zpool should be readonly
    git-svn-id: svn://anonsvn.h3q.com/svn/freebsd-patches@14 1bf41d8a-299c-db11-a312-0030488a8c24

--- ZFS-TM/create_zfs_backup_initial.sh
+++ ZFS-TM/create_zfs_backup_initial.sh
@@ -10,3 +10,4 @@ last_snapshot=`zfs list -H -o name -t snapshot -r $my_pool/$my_zfs|tail -n1`
 echo "zfs create $my_backuppool/$my_pool"
 echo "zfs send -R $last_snapshot  |zfs recv -d $my_backuppool/$my_pool"
 echo "zfs clone $my_backuppool/$last_snapshot $my_backuppool/$my_pool/${my_zfs}_backupmark"
+echo "zfs set readonly=on $my_backuppool/$my_pool"
