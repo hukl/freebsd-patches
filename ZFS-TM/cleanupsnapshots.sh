63a61ca10f5b3cce8dcba434b91b436be28f89ae
cryx <cryx@1bf41d8a-299c-db11-a312-0030488a8c24>
2009-03-10 19:14:01 +0000

    some cleanups of variable names
    git-svn-id: svn://anonsvn.h3q.com/svn/freebsd-patches@22 1bf41d8a-299c-db11-a312-0030488a8c24

--- ZFS-TM/cleanupsnapshots.sh
+++ ZFS-TM/cleanupsnapshots.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
-zfs='tank/home/joe'
-limit='699M'
-current=`zfs list -H -o usedbysnapshots $zfs`
+zfs='backup/data/home/cryx'
+limit='2.0G'
+current=`zfs list -H -o used $zfs`
+data=`zfs list -H -o usedbydataset $zfs`
 
-_limit=`echo "${limit}"|tr GMKBWX gmkbwx|sed -Ees:g:km:g -es:m:kk:g -es:k:"*2b":g -es:b:"*128w":g -es:w:"*4 ":g -e"s:(^|[^0-9])0x:\1\0X:g" -ey:x:"*":|bc`
+_limit=`echo "${limit}"|tr GMKBWX gmkbwx|sed -Ees:g:km:g -es:m:kk:g -es:k:"*2b":g -es:b:"*128w":g -es:w:"*4 ":g -e"s:(^|[^0-9])0x:\1\0X:g" -ey:x:"*":|bc |sed "s:\.[0-9]*$::g"`
+_data=`echo "${data}"|tr GMKBWX gmkbwx|sed -Ees:g:km:g -es:m:kk:g -es:k:"*2b":g -es:b:"*128w":g -es:w:"*4 ":g -e"s:(^|[^0-9])0x:\1\0X:g" -ey:x:"*":|bc |sed "s:\.[0-9]*$::g"`
 
 calc_bytesize () {
-   _current=`echo "${current}"|tr GMKBWX gmkbwx|sed -Ees:g:km:g -es:m:kk:g -es:k:"*2b":g -es:b:"*128w":g -es:w:"*4 ":g -e"s:(^|[^0-9])0x:\1\0X:g" -ey:x:"*":|bc`
+   _current=`echo "${current}"|tr GMKBWX gmkbwx|sed -Ees:g:km:g -es:m:kk:g -es:k:"*2b":g -es:b:"*128w":g -es:w:"*4 ":g -e"s:(^|[^0-9])0x:\1\0X:g" -ey:x:"*":|bc |sed "s:\.[0-9]*$::g"`
+#   _current=`echo "${_current}+${_data}" |bc`
 }
 
 calc_bytesize
 
 while [ "$_current" -gt "$_limit" ]; do
 	delete=`zfs list -H -o name -t snapshot -r $zfs | grep ${zfs}'@' | head -n1`
+	zfs get -H used $delete
 	echo "zfs destroy $delete"
-	sudo zfs destroy $delete
-	sleep 1
-	current=`zfs list -H -o usedbysnapshots $zfs`
+#	sudo zfs destroy $delete
+	current=`zfs list -H -o used $zfs`
 	calc_bytesize
 	echo $_current $_limit
 done
